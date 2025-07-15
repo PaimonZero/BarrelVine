9013ca7ee728fc18df551bbc83440d4162e3e348
congdeptrai0411 <thanhcongnguyen.91.2004@gmail.com>
2025-07-15 18:24:05 +0700

    - done login and register

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const ctrls = require('@controllers/authController');
+const tokenUtils = require('@middlewares/jwt');
+const passport = require('passport');
+
+// Valid types for messages
+const validTypes = ['warning', 'error', 'success', 'info'];
+
+// [GET] auth/register Display login page
+router.get('/login', ctrls.displayLogin);
+// [POST] auth/login Login user
+router.post('/login', ctrls.login);
+// [POST] auth/logout Logout user
+router.post('/logout', tokenUtils.verifyAccessToken, ctrls.logout);
+
+// [GET] auth/register Display register page
+router.get('/register', ctrls.displayRegister);
+// [POST] auth/register Register user
+router.post('/register', ctrls.register);
+
+// [GET] auth/google Redirect to Google for authentication
+router.get('/google', 
+    passport.authenticate('google', { scope: ['profile', 'email'] }),
+    ctrls.googleLogin
+);
+
+// [GET] auth/google/callback Handle Google authentication callback
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect: '/auth/login' }),
+    ctrls.googleCallback
+);
+
+module.exports = router;
