4c894b3fabfe19e3ad74e64702528ddc877afe47
gianglamnguyen <lamgiangvpr123@gmail.com>
2025-07-16 20:29:41 +0700

    customer homepage

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -3,8 +3,13 @@ const productRouter = require('@routes/productRoutes');
 const categoryRouter = require('@routes/categoryRoutes');
 const couponRouter = require('@routes/couponRoutes');
 const invoiceRouter = require('@routes/invoiceRoutes');
+const adminRouter = require('@routes/adminRoutes');
 
+const customerHomePageRouter = require('@routes/customer/homePageRoutes');
+
+const authRouter = require('@routes/authRoutes');
 const { notFound, errorHandler } = require('@middlewares/errorHandler');
+const tokenUtils = require('@middlewares/jwt');
 
 const initRoutes = (app) => {
     // page user
@@ -16,7 +21,14 @@ const initRoutes = (app) => {
     //page coupon
     app.use('/api/coupon', couponRouter);
     // page invoice
-    app.use('/api/invoice', invoiceRouter);    
+    app.use('/api/invoice', invoiceRouter);
+    // page admin
+    app.use('/admin', [tokenUtils.verifyAccessToken, tokenUtils.isAdmin], adminRouter);
+    // page auth
+    app.use('/auth', authRouter);
+
+    // page UI
+    app.use('/', customerHomePageRouter);
 
     // handle 404 - This should be the last route
     app.use(notFound);
