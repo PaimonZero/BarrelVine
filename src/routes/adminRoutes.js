de38ab68c46a95de63ae3c627c623b214c95690a
ZeroPaimon <pnam2212004@gmail.com>
2025-07-20 08:56:14 +0700

    done admin order

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -3,9 +3,10 @@ const tokenUtils = require('@middlewares/jwt');
 const uploadCloud = require('@config/cloudinary.config');
 const ctrlsUser = require('@controllers/admin/userAdmin');
 const ctrlsDashboard = require('@controllers/admin/dashboardAdmin');
-const ctrlsCategory = require('@controllers/admin/categoryAdmin');
 const ctrlsProduct = require('@controllers/admin/productAdmin');
 const ctrlsCoupon = require('@controllers/admin/couponAdmin');
+const ctrlsCategory = require('@controllers/admin/categoryAdmin');
+const ctrlsInvoice = require('@controllers/admin/orderAdmin');
 
 // test controller
 const Product = require('@models/Product');
@@ -30,15 +31,6 @@ router.get('/users', ctrlsUser.renderUsersPage);
 router.post('/user-create', ctrlsUser.createUser);
 // Route to update user
 router.post('/user-update', ctrlsUser.updateUserInAdminPage);
-// ______ Category Management Routes ______
-// Route to view categories
-router.get('/categories', ctrlsCategory.renderCategoriesPage);
-// Route to create category
-router.post('/category-create', ctrlsCategory.createCategory);
-// Route to update category
-router.post('/category-update/:cid', ctrlsCategory.updateCategory);
-// Route to delete category
-router.post('/category-delete/:cid', ctrlsCategory.deleteCategory);
 
 // ______ Product Management Routes ______
 // Route to view products
@@ -65,5 +57,27 @@ router.post('/coupon-create', ctrlsCoupon.createCoupon);
 router.post('/coupon-update/:cid', ctrlsCoupon.updateCoupon);
 // Route to delete coupon
 router.post('/coupon-delete/:cid', ctrlsCoupon.deleteCoupon);
+
+// ______ Category Management Routes ______
+// Route to view categories
+router.get('/categories', ctrlsCategory.renderCategoriesPage);
+// Route to create category
+router.post('/category-create', ctrlsCategory.createCategory);
+// Route to update category
+router.post('/category-update/:cid', ctrlsCategory.updateCategory);
+// Route to delete category
+router.post('/category-delete/:cid', ctrlsCategory.deleteCategory);
+
+//______ Order Management Routes ______
+// Route to view orders
+router.get('/orders', ctrlsInvoice.renderOrdersPage);
+// Route to view order details
+router.get('/order-details/:id', ctrlsInvoice.renderOrderDetailsPage);
+// Route to update status
+router.post('/order-update/status/:id', ctrlsInvoice.updateOrderStatus);
+// Route to delete order
+router.post('/order-delete/:id', ctrlsInvoice.deleteOrder);
+// Route to create order
+router.post('/order-create', ctrlsInvoice.createNewOrder);
 
 module.exports = router;
